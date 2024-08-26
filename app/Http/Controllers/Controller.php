a690085657fd269b1b46c98197d0a71c348735b9
Schmetterling-cd <vladrb2002@gmail.com>
2024-08-26 07:48:06 +0300

    Универсализация ответа API

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,14 +11,21 @@ abstract class Controller
     public function getApiResponse($data, string $message = ''): array
     {
 
+        $response = [];
+
+        if (!empty($message)) {
+            $response['message'] = $message;
+        }
+
         if (is_bool($data)) {
-            $response = ['status' => $data];
+            $response['status'] = $data;
+            return $response;
         } else {
             $response = ['data' => $data];
         }
 
-        if (!empty($message)) {
-            $response['message'] = $message;
+        if (empty($response['status'])) {
+            $response['status'] = true;
         }
 
         return $response;
